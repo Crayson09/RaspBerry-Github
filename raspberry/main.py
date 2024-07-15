40013628f66479b17d19d87784d2c07c3d560ec4
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 10:26:39 +0200

    v15

--- raspberry/main.py
+++ raspberry/main.py
@@ -5,21 +5,18 @@ import random
 led_green = LED(19)
 button = Button(2)
 button_2 = Button(4)
+random_time = random.randint(15,30)
 def button_pressed():
     print('2')
-    led_green.on()
-    sleep(2)
-    led_green.off()
+
 
 
 def button_pressed_2():
     print("1")
-    led_green.on()
-    sleep(2)
-    led_green.off()
+
 
 button.when_pressed = button_pressed
 button_2.when_pressed = button_pressed_2
-# Main loop to keep the program running
 while True:
-    sleep(1)
+    sleep(random_time)
+    led_green.on()
