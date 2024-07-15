5ed595e13fbea96aefc9ed9244a162d1b713b5ee
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 10:04:18 +0200

    v9

--- raspberry/main.py
+++ raspberry/main.py
@@ -6,11 +6,10 @@ led_red = LED(26)
 led_green = LED(19)
 led_yellow = LED(13)
 button = Button(5)
-button.when_pressed = led_yellow.on
 button.when_released = led_yellow.off
-led_red.on()
 if button.is_held:
-    sleep(5)
+    print("Pressed")
+    sleep(2)
     led_red.off()
     led_yellow.on()
     sleep(1)
