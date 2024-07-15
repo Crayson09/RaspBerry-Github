36f1451b1eb0daccca87b576e05e683f10f4a998
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 09:29:05 +0200

    v3

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,1 +1,16 @@
-print("Hello World")
+# Bibliotheken laden
+from gpiozero import LED
+from time import sleep
+
+# Initialisierung von GPIO17 als LED (Ausgang)
+led = LED(26)
+
+# LED einschalten
+led.on()
+print("Licht an)")
+# 5 Sekunden warten
+sleep(5)
+
+# LED ausschalten
+led.off()
+print("Licht aus")
