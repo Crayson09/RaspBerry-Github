b2f795edb65fb16107fcd5dd477528de135dbe89
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 11:21:05 +0200

    Button Cam Foto

--- raspberry/cam.py
+++ raspberry/cam.py
@@ -1,10 +1,25 @@
 from picamera2 import Picamera2, Preview
 from time import sleep
+from gpiozero import LED, Button
+button = Button(2)
 
-picam2 = Picamera2()
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.QTGL)
-picam2.start()
-sleep(2)
-picam2.capture_file("test.jpg")
+
+def button_pressed():
+    print("Gedrückt")
+    picam2 = Picamera2()
+    camera_config = picam2.create_preview_configuration()
+    picam2.configure(camera_config)
+    picam2.start_preview(Preview.QTGL)
+    picam2.start()
+    sleep(2)
+    picam2.capture_file("button_pressed.jpg")
+
+
+button.when_pressed = button_pressed
+
+
+
+
+while True:
+    sleep(1)
+
