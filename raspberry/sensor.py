4543bd59d389eff5ca4da46774f3b161a8bb80bd
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 15:14:17 +0200

    Update sensor.py

--- raspberry/sensor.py
+++ raspberry/sensor.py
@@ -5,21 +5,11 @@ GPIO.setmode(GPIO.BCM)
 PIR_PIN = 20
 GPIO.setup(PIR_PIN, GPIO.IN)
 
-def MOTION(PIR_PIN):
-    print("Motion Detected!")
-
-print("PIR Module Test (CTRL+C to exit)")
-time.sleep(2)
-print("Ready")
-
 try:
-    GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
     while True:
-        time.sleep(100)
-
+        if GPIO.input(PIR_PIN):
+            print("Motion Detected!")
+        time.sleep(1)
 except KeyboardInterrupt:
     print("Quit")
-    GPIO.cleanup()
-except RuntimeError as e:
-    print(f"RuntimeError: {e}")
     GPIO.cleanup()
