deb530069fed9b76ee7ad63fe80de62082c48109
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 10:01:03 +0200

    v7

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,22 +1,24 @@
 # Bibliotheken laden
-from gpiozero import LED
+from gpiozero import LED,Button
 from time import sleep
 
 led_red = LED(26)
 led_green = LED(19)
 led_yellow = LED(13)
+button = Button(5)
 
 led_red.on()
-sleep(5)
-led_red.off()
-led_yellow.on()
-sleep(1)
-led_yellow.off()
-led_red.off()
-led_green.on()
-sleep(5)
-led_yellow.on()
-sleep(1)
-led_yellow.off()
-led_red.on()
-led_green.off()
+if button.is_held:
+    sleep(5)
+    led_red.off()
+    led_yellow.on()
+    sleep(1)
+    led_yellow.off()
+    led_red.off()
+    led_green.on()
+    sleep(5)
+    led_yellow.on()
+    sleep(1)
+    led_yellow.off()
+    led_red.on()
+    led_green.off()
