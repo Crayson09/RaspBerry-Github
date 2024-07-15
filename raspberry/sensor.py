07c3e2151228d58d514c26917c9333e3f4d882a4
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 14:49:21 +0200

    Update sensor.py

--- raspberry/sensor.py
+++ raspberry/sensor.py
@@ -1,9 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 
+from gpiozero import LED
+
 GPIO.setmode(GPIO.BOARD)
 PIN = 21
 GPIO.setup(PIN, GPIO.IN)
+led = LED(20)
 
 print ("HC-SR505 Bewegungssensor starten...")
 time.sleep(2)
@@ -12,5 +15,6 @@ print ("Bewegungssensor aktiviert...")
 while True:
    if GPIO.input(PIN):
       print ("Bewegung erkannt am " + (time.strftime("%H:%M:%S")))
+      led.blink()
       time.sleep(9)
    time.sleep(1)
