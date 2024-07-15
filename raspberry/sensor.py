f3f6458738de0788ab3c9f67c2d1128fa6c8e2f6
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 14:42:45 +0200

    Update sensor.py

--- raspberry/sensor.py
+++ raspberry/sensor.py
@@ -1,53 +1,16 @@
-#!/usr/bin/python
-
-# Import der Python libraries
 import RPi.GPIO as GPIO
 import time
-import datetime
 
-# Wir verwenden den Board Mode, Angabe der PIN Nummern anstelle der GPIO BCM Nummer
 GPIO.setmode(GPIO.BOARD)
+PIN = 21
+GPIO.setup(PIN, GPIO.IN)
 
-# GPIO definieren, 7 da bei mir der Sensor auf Pin7 steckt
-GPIO_PIR = 13
+print ("Start HC-SR505 bewegingssensor...")
+time.sleep(2)
+print ("Bewegingssensor geactiveerd...")
 
-print("Bewegungsmelder Test (CTRL-C zum Beenden)")
-print("=========================================")
-
-#  GPIO als "Input" festlegen
-GPIO.setup(GPIO_PIR,GPIO.IN)
-
-Current_State  = 0
-Previous_State = 0
-
-try:
-
- print("%s: Sensor initialisieren ..." % datetime.datetime.now())
-
- # Warten bis Sensor sich meldet
- while GPIO.input(GPIO_PIR)==1:
-   Current_State  = 0
-
- print ("%s: Fertig! Warte auf Bewegung..."  % datetime.datetime.now())
-
- # Schleife bis CTRL+C
- while True :
-
-   #Status von Sensor auslesen
-   Current_State = GPIO.input(GPIO_PIR)
-
-   if Current_State==1 and Previous_State==0:
-
-     print(" %s: Bewegung erkannt!" % datetime.datetime.now())
-     Previous_State=1
-
-   elif Current_State==0 and Previous_State==1:
-
-     print(" %s: Fertig! Warte auf Bewegung..."  % datetime.datetime.now())
-     Previous_State=0
-
-   time.sleep(0.01)
-
-except KeyboardInterrupt:
- print(" Exit")
- GPIO.cleanup()
+while True:
+   if GPIO.input(PIN):
+      print "(Beweging gedetecteerd op " + (time.strftime("%H:%M:%S")))
+      time.sleep(9)
+   time.sleep(1)
