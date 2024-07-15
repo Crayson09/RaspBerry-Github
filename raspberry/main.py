838fb1df0cf686618a3a0519b1feec62cae3100d
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 13:54:51 +0200

    test

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,7 +1,6 @@
 from gpiozero import LED, Button
 from time import sleep
 import random
-from picamera2 import Picamera2, Preview
 
 # Initialisierung
 led_green = LED(19)
@@ -10,18 +9,13 @@ button_2 = Button(4)
 random_time = random.randint(5, 20)
 
 
-
-
-
-
-
-
 # Variable für den LED-Zustand und Programmstatus
 leuchtet = False
 running = True
 
 def button_pressed():
     global leuchtet, running
+    print("Pressed 1")
     if leuchtet:
         led_green.off()
         print("Spieler 2 hat gewonnen")
@@ -30,6 +24,7 @@ def button_pressed():
 
 def button_pressed_2():
     global leuchtet, running
+    print("Pressed 1")
     if leuchtet:
         led_green.off()
         print("Spieler 1 hat gewonnen")
