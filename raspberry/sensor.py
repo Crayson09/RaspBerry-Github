917568be31c9e4e5943084587a6ef6409413452d
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 15:33:45 +0200

    Update sensor.py

--- raspberry/sensor.py
+++ raspberry/sensor.py
@@ -12,15 +12,14 @@ try:
     while True:
         if GPIO.input(PIR_PIN):
             print("Motion Detected!")
+            led.on()
             picam2 = Picamera2()
             camera_config = picam2.create_preview_configuration()
             picam2.configure(camera_config)
-            picam2.start_preview(Preview.QTGL)
             picam2.start()
             time.sleep(3)
             picam2.capture_file("einbrecher.jpg")
-            led.on()
-            time.sleep(1)
+            print("Motion Detected")
             led.off()
             time.sleep(8)
         time.sleep(1)
