a12b5d0d0152ab3b14102b54415422ffebb10bf5
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 13:22:01 +0200

    Update foto.py

--- raspberry/foto.py
+++ raspberry/foto.py
@@ -1,11 +1,10 @@
 from picamera2 import Picamera2, Preview
 import time
 
-def foto():
-    picam2 = Picamera2()
-    camera_config = picam2.create_preview_configuration()
-    picam2.configure(camera_config)
-    picam2.start_preview(Preview.QTGL)
-    picam2.start()
-    time.sleep(5)
-    picam2.capture_file("image.jpg")
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(5)
+picam2.capture_file("image.jpg")
