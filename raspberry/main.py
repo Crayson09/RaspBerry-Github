a9da8d39f8114c175a27c0b321277183bcb0e3af
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 10:29:04 +0200

    v16

--- raspberry/main.py
+++ raspberry/main.py
@@ -5,14 +5,21 @@ import random
 led_green = LED(19)
 button = Button(2)
 button_2 = Button(4)
-random_time = random.randint(15,30)
+random_time = random.randint(5,20)
+
 def button_pressed():
     print('2')
+    if leuchtet:
+        led_green.off()
+        print("Spieler 2 hat gewonnen")
 
 
 
 def button_pressed_2():
     print("1")
+    if leuchtet:
+        led_green.off()
+        print("Spieler 1 hat gewonnen")
 
 
 button.when_pressed = button_pressed
@@ -20,3 +27,4 @@ button_2.when_pressed = button_pressed_2
 while True:
     sleep(random_time)
     led_green.on()
+    leuchtet = True
