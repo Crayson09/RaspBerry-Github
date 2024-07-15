0aad2b011175b02a3a886867381f066b8137747e
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 10:24:45 +0200

    v14

--- raspberry/main.py
+++ raspberry/main.py
@@ -1,19 +1,25 @@
 # Bibliotheken laden
 from gpiozero import LED, Button
 from time import sleep
-
+import random
 led_green = LED(19)
 button = Button(2)
-
+button_2 = Button(4)
 def button_pressed():
-    print('You pushed me')
-    print("Pressed")
+    print('2')
+    led_green.on()
+    sleep(2)
+    led_green.off()
+
+
+def button_pressed_2():
+    print("1")
     led_green.on()
     sleep(2)
     led_green.off()
 
 button.when_pressed = button_pressed
-
+button_2.when_pressed = button_pressed_2
 # Main loop to keep the program running
 while True:
     sleep(1)
