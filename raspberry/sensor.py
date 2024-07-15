4105330b540705d5317fec5cc5cc3866bf56dd6e
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 15:21:06 +0200

    Update sensor.py

--- raspberry/sensor.py
+++ raspberry/sensor.py
@@ -1,15 +1,19 @@
 import RPi.GPIO as GPIO
 import time
 
+from gpiozero import LED
+
 GPIO.setmode(GPIO.BCM)
 PIR_PIN = 20
 GPIO.setup(PIR_PIN, GPIO.IN)
-
+led = LED(21)
 try:
     while True:
         if GPIO.input(PIR_PIN):
             print("Motion Detected!")
+            led.on()
             time.sleep(10)
+            led.off()
         time.sleep(1)
 except KeyboardInterrupt:
     print("Quit")
