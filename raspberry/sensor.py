a7003aa56443a199b6ae620e90d8d5ba909e01f9
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-15 14:09:28 +0200

    Update sensor.py

--- raspberry/sensor.py
+++ raspberry/sensor.py
@@ -1,20 +1,53 @@
+#!/usr/bin/python
+
+# Import der Python libraries
 import RPi.GPIO as GPIO
 import time
+import datetime
 
-SENSOR_PIN = 13
+# Wir verwenden den Board Mode, Angabe der PIN Nummern anstelle der GPIO BCM Nummer
+GPIO.setmode(GPIO.BOARD)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(SENSOR_PIN, GPIO.IN)
+# GPIO definieren, 7 da bei mir der Sensor auf Pin7 steckt
+GPIO_PIR = 13
 
-def mein_callback(channel):
-    print('Es gab eine Bewegung!')
+print("Bewegungsmelder Test (CTRL-C zum Beenden)")
+print("=========================================")
+
+#  GPIO als "Input" festlegen
+GPIO.setup(GPIO_PIR,GPIO.IN)
+
+Current_State  = 0
+Previous_State = 0
 
 try:
-    GPIO.add_event_detect(SENSOR_PIN, GPIO.RISING, callback=mein_callback)
-    print('Warte auf Bewegung...')
-    while True:
-        time.sleep(1)  # Reduced sleep time for responsiveness
+
+ print("%s: Sensor initialisieren ..." % datetime.datetime.now())
+
+ # Warten bis Sensor sich meldet
+ while GPIO.input(GPIO_PIR)==1:
+   Current_State  = 0
+
+ print ("%s: Fertig! Warte auf Bewegung..."  % datetime.datetime.now())
+
+ # Schleife bis CTRL+C
+ while True :
+
+   #Status von Sensor auslesen
+   Current_State = GPIO.input(GPIO_PIR)
+
+   if Current_State==1 and Previous_State==0:
+
+     print(" %s: Bewegung erkannt!" % datetime.datetime.now())
+     Previous_State=1
+
+   elif Current_State==0 and Previous_State==1:
+
+     print(" %s: Fertig! Warte auf Bewegung..."  % datetime.datetime.now())
+     Previous_State=0
+
+   time.sleep(0.01)
+
 except KeyboardInterrupt:
-    print("\nBeende...")
-finally:
-    GPIO.cleanup()
+ print(" Exit")
+ GPIO.cleanup()
