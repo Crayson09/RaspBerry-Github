c69f33a25f88631b686120627c35c0c1fdd8232b
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 10:24:18 +0200

    Create password.py

---
+++ raspberry/password.py
@@ -0,0 +1,30 @@
+from gpiozero import LED, Button
+from time import sleep
+import random
+
+# Initialisierung
+led_green = LED(19)
+button = Button(2)
+button_2 = Button(3)
+button_3 = Button(4)
+
+def button_pressed():
+    print("Pressed:1")
+
+
+def button_pressed_2():
+    print("Pressed:1")
+
+
+def button_pressed_3():
+    print("Pressed:3")
+# Event-Handler zuweisen
+button.when_pressed = button_pressed
+button_2.when_pressed = button_pressed_2
+button_3.when_pressed = button_pressed_3
+# Hauptschleife
+while True:
+    sleep(1)
+
+
+
