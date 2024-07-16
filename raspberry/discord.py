5e2d126df077faf48dcba88bda40c6308683504c
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 09:39:12 +0200

    Update discord.py

--- raspberry/discord.py
+++ raspberry/discord.py
@@ -1,8 +1,6 @@
 
 
 import discord
-from discord import ui, app_commands
-from discord.app_commands import Choice
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), status=discord.Status.online)
