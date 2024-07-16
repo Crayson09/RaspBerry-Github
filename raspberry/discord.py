7921ad5d05b830bc6eff8832a5959b075330d6a0
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 09:25:57 +0200

    Create discord.py

---
+++ raspberry/discord.py
@@ -0,0 +1,10 @@
+
+
+import discord
+from discord import ui, app_commands
+from discord.app_commands import Choice
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), status=discord.Status.online)
+
+bot.run("MTI2MjY3MTQ1NTM0NzI4MjAwMA.Gs71Rt.NGGPM2a4crEOiE9VQWas77cjfOZscP0UU4qPFI")
