fa3d176af47f587794a3326970ee8888988f0f26
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 09:44:21 +0200

    Create time.py

---
+++ raspberry/time.py
@@ -0,0 +1,14 @@
+import schedule
+import time
+
+# Funktion zum Ausgeben der Nachricht
+def print_message():
+    print("Hier ist deine automatische Nachricht!")
+
+# Zeitplan festlegen (Beispiel: Jeden Tag um 8:00 Uhr)
+schedule.every().day.at("08:00").do(print_message)
+
+# Endlosschleife zum Überprüfen des Zeitplans
+while True:
+    schedule.run_pending()
+    time.sleep(1)
