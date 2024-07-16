752bd65e04395886692071359abeb486e5f6f702
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 10:43:03 +0200

    Update password.py

--- raspberry/password.py
+++ raspberry/password.py
@@ -1,6 +1,5 @@
 from gpiozero import LED, Button
 from time import sleep
-import random
 
 # Initialisierung
 led_green = LED(19)
@@ -8,20 +7,39 @@ button = Button(2)
 button_2 = Button(3)
 button_3 = Button(4)
 
-def button_pressed():
-    print("Pressed:1")
+# Festlegen der korrekten Reihenfolge der Tastendrücke
+correct_sequence = [1, 1, 3, 2]
+current_sequence = []
 
 
-def button_pressed_2():
-    print("Pressed:2")
+def check_sequence():
+    global current_sequence
+
+    if current_sequence == correct_sequence:
+        print("YES")
+        # Hier kannst du weitere Aktionen ausführen, z.B. LED einschalten
+        led_green.on()
+        current_sequence.clear()
+    elif len(current_sequence) >= 4:
+        print("falsch")
+        current_sequence.clear()
 
 
-def button_pressed_3():
-    print("Pressed:3")
 # Event-Handler zuweisen
-button.when_pressed = button_pressed
-button_2.when_pressed = button_pressed_2
-button_3.when_pressed = button_pressed_3
+def button_pressed(button_num):
+    global current_sequence
+    current_sequence.append(button_num)
+    check_sequence()
+
+
+button.when_pressed = lambda: button_pressed(1)
+button_2.when_pressed = lambda: button_pressed(2)
+button_3.when_pressed = lambda: button_pressed(3)
+
 # Hauptschleife
-while True:
-    sleep(1)
+try:
+    while True:
+        sleep(1)
+
+finally:
+    led_green.off()  # LED ausschalten, wenn das Programm beendet wird
