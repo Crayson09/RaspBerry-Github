88310b95e807fd0097ebfb37d528489d36f584c4
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 09:41:19 +0200

    Update discord.py

--- raspberry/discord.py
+++ raspberry/discord.py
@@ -1,8 +1,19 @@
-
-
 import discord
-from discord.ext import commands
 
-bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), status=discord.Status.online)
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = MyClient(intents=intents)
 
 bot.run("")
