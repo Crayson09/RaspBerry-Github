478871667fd75433ef5f2db4e3d9999c4e88b3dc
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 09:32:03 +0200

    Update discord.py

--- raspberry/discord.py
+++ raspberry/discord.py
@@ -7,4 +7,4 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all(), status=discord.Status.online)
 
-bot.run("MTI2MjY3MTQ1NTM0NzI4MjAwMA.Gs71Rt.NGGPM2a4crEOiE9VQWas77cjfOZscP0UU4qPFI")
+bot.run("MTI2MjY3MTQ1NTM0NzI4MjAwMA.GK6r2O.VI5d-2Swz9hpBmNSNasytkz5Azm5JSW53o6n-8")
