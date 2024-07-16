41e301e56876f30c797ef42f21d35664e0c9a5d7
Stylo09 <122814578+Stylo09@users.noreply.github.com>
2024-07-16 10:05:50 +0200

    Update time.py

--- raspberry/time.py
+++ raspberry/time.py
@@ -1,10 +1,22 @@
 import argparse
 import schedule
 import time
+import requests
 
-# Funktion zum Ausgeben der Nachricht mit variablem Inhalt
-def print_custom_message(content):
-    print(f"{content}")
+# Discord Webhook URL einfügen
+DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1262681444568928298/D0ntYqkalDdLVD6An7XLoSlzK-7qPeLmpu5Mq76ws07J39dHtwTX4r2LktK1J0h0Kmvs'
+
+# Funktion zum Senden der Nachricht über Discord-Webhook
+def send_discord_message(content):
+    data = {
+        'content': content
+    }
+    try:
+        response = requests.post(DISCORD_WEBHOOK_URL, json=data)
+        response.raise_for_status()
+        print('Nachricht erfolgreich über Discord-Webhook gesendet!')
+    except requests.exceptions.RequestException as e:
+        print(f'Fehler beim Senden der Nachricht über Discord-Webhook: {e}')
 
 # Funktion zum Hinzufügen neuer Zeitpläne
 def add_new_schedule(day, hour, minute, job, content):
@@ -12,7 +24,7 @@ def add_new_schedule(day, hour, minute, job, content):
 
 # Funktion zum Parsen von Kommandozeilenargumenten
 def parse_arguments():
-    parser = argparse.ArgumentParser(description="Fügt einen neuen Zeitplan für eine automatische Nachricht hinzu.")
+    parser = argparse.ArgumentParser(description="Fügt einen neuen Zeitplan für eine automatische Nachricht über Discord hinzu.")
     parser.add_argument("day", choices=["every", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"], help="Tag des Zeitplans")
     parser.add_argument("hour", type=int, help="Stunde des Zeitplans (0-23)")
     parser.add_argument("minute", type=int, help="Minute des Zeitplans (0-59)")
@@ -21,7 +33,7 @@ def parse_arguments():
 
 if __name__ == "__main__":
     args = parse_arguments()
-    add_new_schedule(args.day, args.hour, args.minute, print_custom_message, args.content)
+    add_new_schedule(args.day, args.hour, args.minute, send_discord_message, args.content)
 
     # Endlosschleife zum Überprüfen des Zeitplans
     while True:
